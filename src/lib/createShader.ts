a4be3c888ca8f6cce138e8101b5bad6dce3957a9
Julien Sulpis <julien.sulpis@gmail.com>
2024-09-06 22:02:49 +0200

    feat: convert GLSL 100 shader source to GLSL 300 to handle both versions

--- src/lib/createShader.ts
+++ src/lib/createShader.ts
@@ -1,6 +1,6 @@
 export function createShader(gl: WebGL2RenderingContext, source: string, type: GLenum) {
 	const shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
+	gl.shaderSource(shader, convertToGLSL300(source));
 	gl.compileShader(shader);
 
 	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -8,4 +8,30 @@ export function createShader(gl: WebGL2RenderingContext, source: string, type: G
 		gl.deleteShader(shader);
 	}
 	return shader;
+}
+
+function convertToGLSL300(shader: string): string {
+	let glsl300Shader = shader.replace(/\battribute\b/g, "in").replace(/\btexture2D\b/g, "texture");
+
+	if (shader.includes("gl_FragColor")) {
+		// Fragment shader
+		glsl300Shader = "out vec4 fragColor;\n" + glsl300Shader;
+		glsl300Shader = glsl300Shader
+			.replace(/\bvarying\b/g, "in")
+			.replace(/\bgl_FragColor\b/g, "fragColor");
+	} else {
+		// Vertex shader
+		glsl300Shader = glsl300Shader.replace(/\bvarying\b/g, "out");
+	}
+
+	const precisionRegex = /precision\s+(highp|mediump|lowp)\s+float\s*;/;
+	if (!precisionRegex.test(glsl300Shader)) {
+		glsl300Shader = glsl300Shader.replace(/^(#version 300 es)?/, "$1\nprecision highp float;\n");
+	}
+
+	if (!shader.startsWith("#version")) {
+		glsl300Shader = "#version 300 es\n" + glsl300Shader;
+	}
+
+	return glsl300Shader;
 }
