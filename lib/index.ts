1857c1cae2f7b8d548e1ccea98d4ac10ae37824f
Julien Sulpis <code@jsulpis.dev>
2024-09-27 22:06:47 +0200

    chore: separate packages for docs and lib

---
+++ lib/index.ts
@@ -0,0 +1,17 @@
+export * from "./src/core/attribute";
+export * from "./src/core/buffer";
+export * from "./src/core/program";
+export * from "./src/core/renderTarget";
+export * from "./src/core/shader";
+export * from "./src/core/texture";
+
+export * from "./src/hooks/useCompositor";
+export * from "./src/hooks/useEffectPass";
+export * from "./src/hooks/useQuadRenderPass";
+export * from "./src/hooks/useRenderPass";
+export * from "./src/hooks/useWebGLCanvas";
+export * from "./src/hooks/useWebGLContext";
+
+export * from "./src/helpers/loop";
+export * from "./src/helpers/pointer";
+export * from "./src/helpers/resize";
