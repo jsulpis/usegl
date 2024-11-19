ec7524df0679aa084ad4048aace633919533f5d4
Julien Sulpis <code@jsulpis.dev>
2024-11-19 23:21:43 +0100

    docs: create documentation with Astro and Starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			plugins: [
+				starlightUtils({
+					navLinks: {
+						leading: { useSidebarLabelled: "leadingNavLinks" },
+					},
+				}),
+			],
+			title: "useGL",
+			social: {
+				github: "https://github.com/jsulpis/usegl",
+				blueSky: "https://bsky.app/profile/jsulpis.dev",
+				"x.com": "https://x.com/jsulpis",
+			},
+			pagination: false,
+			sidebar: [
+				{
+					label: "leadingNavLinks",
+					items: [
+						{ label: "Guides", link: "/introduction/quick-start" },
+						{ label: "Examples", link: "/examples/gradient" },
+					],
+				},
+				{
+					label: "Introduction",
+					autogenerate: { directory: "introduction" },
+				},
+			],
+		}),
+	],
+});
