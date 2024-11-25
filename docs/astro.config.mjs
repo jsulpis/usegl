7e084a03e3344025a9c70352063f62aef5063ff7
Julien Sulpis <code@jsulpis.dev>
2024-11-25 23:03:33 +0100

    docs: setup interactive examples

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,38 +2,52 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightUtils from "@lorenzo_lewis/starlight-utils";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [
-				starlightUtils({
-					navLinks: {
-						leading: { useSidebarLabelled: "leadingNavLinks" },
-					},
-				}),
-			],
-			title: "useGL",
-			social: {
-				github: "https://github.com/jsulpis/usegl",
-				blueSky: "https://bsky.app/profile/jsulpis.dev",
-				"x.com": "https://x.com/jsulpis",
-			},
-			pagination: false,
-			sidebar: [
-				{
-					label: "leadingNavLinks",
-					items: [
-						{ label: "Guides", link: "/introduction/quick-start" },
-						{ label: "Examples", link: "/examples/gradient" },
-					],
-				},
-				{
-					label: "Introduction",
-					autogenerate: { directory: "introduction" },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightUtils({
+          navLinks: {
+            leading: { useSidebarLabelled: "leadingNavLinks" },
+          },
+        }),
+      ],
+      title: "useGL",
+      social: {
+        github: "https://github.com/jsulpis/usegl",
+        blueSky: "https://bsky.app/profile/jsulpis.dev",
+        "x.com": "https://x.com/jsulpis",
+      },
+      pagination: false,
+      sidebar: [
+        {
+          label: "leadingNavLinks",
+          items: [
+            { label: "Guides", link: "/introduction/quick-start" },
+            { label: "Examples", link: "/examples/basics/gradient" },
+          ],
+        },
+        {
+          label: "Introduction",
+          autogenerate: { directory: "introduction" },
+        },
+      ],
+      customCss: ["./src/styles/custom.scss"],
+    }),
+    react(),
+    mdx(),
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+      },
+    },
+  },
 });
