170cd409adb0117a15574c9d3316313fed3509b1
Julien Sulpis <code@jsulpis.dev>
2024-11-26 22:55:06 +0100

    feat: allow crossorigin textures

--- lib/src/core/texture.ts
+++ lib/src/core/texture.ts
@@ -1,53 +1,53 @@
 export type TextureData = ArrayBufferView | null;
 
 export type TextureOptions = {
-	/**
-	 * @default "linear-mipmap-linear" if generateMipmaps is true, "linear" otherwise
-	 */
-	minFilter?: MinFilter;
-	/**
-	 * @default "linear"
-	 */
-	magFilter?: MagFilter;
-	/**
-	 * @default "clamp-to-edge"
-	 */
-	wrapS?: WrappingMode;
-	/**
-	 * @default "clamp-to-edge"
-	 */
-	wrapT?: WrappingMode;
-	/**
-	 * @default true if src is defined, false otherwise
-	 */
-	generateMipmaps?: boolean;
-	/**
-	 * @default navigator.hardwareConcurrency * 2 if src is defined, 0 otherwise
-	 */
-	anisotropy?: number;
-	/**
-	 * @default true if src is defined, false otherwise
-	 */
-	flipY?: boolean;
-	/**
-	 * @default 0
-	 */
-	level?: number;
-	/**
-	 * @default WebGL2RenderingContext.RGBA
-	 */
-	internalFormat?: number;
-	/**
-	 * @default WebGL2RenderingContext.RGBA
-	 */
-	format?: number;
-	/**
-	 * @default WebGL2RenderingContext.UNSIGNED_BYTE
-	 */
-	type?: number;
+  /**
+   * @default "linear-mipmap-linear" if generateMipmaps is true, "linear" otherwise
+   */
+  minFilter?: MinFilter;
+  /**
+   * @default "linear"
+   */
+  magFilter?: MagFilter;
+  /**
+   * @default "clamp-to-edge"
+   */
+  wrapS?: WrappingMode;
+  /**
+   * @default "clamp-to-edge"
+   */
+  wrapT?: WrappingMode;
+  /**
+   * @default true if src is defined, false otherwise
+   */
+  generateMipmaps?: boolean;
+  /**
+   * @default navigator.hardwareConcurrency * 2 if src is defined, 0 otherwise
+   */
+  anisotropy?: number;
+  /**
+   * @default true if src is defined, false otherwise
+   */
+  flipY?: boolean;
+  /**
+   * @default 0
+   */
+  level?: number;
+  /**
+   * @default WebGL2RenderingContext.RGBA
+   */
+  internalFormat?: number;
+  /**
+   * @default WebGL2RenderingContext.RGBA
+   */
+  format?: number;
+  /**
+   * @default WebGL2RenderingContext.UNSIGNED_BYTE
+   */
+  type?: number;
 } & (
-	| { data: TextureData; src?: never; width: number; height: number }
-	| { data?: never; src: TexImageSource }
+  | { data: TextureData; src?: never; width: number; height: number }
+  | { data?: never; src: TexImageSource }
 );
 
 type MagFilter = "linear" | "nearest";
@@ -55,101 +55,106 @@ type MinFilter = "linear" | "nearest" | "linear-mipmap-linear" | "nearest-mipmap
 type WrappingMode = "clamp-to-edge" | "repeat" | "mirrored-repeat";
 
 const minFilterMap: Record<MinFilter, number> = {
-	linear: WebGL2RenderingContext.LINEAR,
-	nearest: WebGL2RenderingContext.NEAREST,
-	"linear-mipmap-linear": WebGL2RenderingContext.LINEAR_MIPMAP_LINEAR,
-	"nearest-mipmap-linear": WebGL2RenderingContext.NEAREST_MIPMAP_LINEAR,
+  linear: WebGL2RenderingContext.LINEAR,
+  nearest: WebGL2RenderingContext.NEAREST,
+  "linear-mipmap-linear": WebGL2RenderingContext.LINEAR_MIPMAP_LINEAR,
+  "nearest-mipmap-linear": WebGL2RenderingContext.NEAREST_MIPMAP_LINEAR,
 };
 
 const magFilterMap: Record<MagFilter, number> = {
-	linear: WebGL2RenderingContext.LINEAR,
-	nearest: WebGL2RenderingContext.NEAREST,
+  linear: WebGL2RenderingContext.LINEAR,
+  nearest: WebGL2RenderingContext.NEAREST,
 };
 
 const wrapMap: Record<WrappingMode, number> = {
-	"clamp-to-edge": WebGL2RenderingContext.CLAMP_TO_EDGE,
-	repeat: WebGL2RenderingContext.REPEAT,
-	"mirrored-repeat": WebGL2RenderingContext.MIRRORED_REPEAT,
+  "clamp-to-edge": WebGL2RenderingContext.CLAMP_TO_EDGE,
+  repeat: WebGL2RenderingContext.REPEAT,
+  "mirrored-repeat": WebGL2RenderingContext.MIRRORED_REPEAT,
 };
 
 export function createTexture(gl: WebGL2RenderingContext, options: TextureOptions) {
-	const {
-		data = null,
-		level = 0,
-		flipY = true,
-		internalFormat = WebGL2RenderingContext.RGBA,
-		format = WebGL2RenderingContext.RGBA,
-		type = WebGL2RenderingContext.UNSIGNED_BYTE,
-		generateMipmaps = true,
-		anisotropy = navigator.hardwareConcurrency, // in most case between 4 and 12, depending on the hardware range
-		minFilter = generateMipmaps ? "linear-mipmap-linear" : "linear",
-		magFilter = "linear",
-		wrapS = "clamp-to-edge",
-		wrapT = "clamp-to-edge",
-	} = options || {};
+  const {
+    data = null,
+    level = 0,
+    flipY = true,
+    internalFormat = WebGL2RenderingContext.RGBA,
+    format = WebGL2RenderingContext.RGBA,
+    type = WebGL2RenderingContext.UNSIGNED_BYTE,
+    generateMipmaps = true,
+    anisotropy = navigator.hardwareConcurrency, // in most case between 4 and 12, depending on the hardware range
+    minFilter = generateMipmaps ? "linear-mipmap-linear" : "linear",
+    magFilter = "linear",
+    wrapS = "clamp-to-edge",
+    wrapT = "clamp-to-edge",
+  } = options || {};
 
-	const boundTexture = gl.getParameter(gl.TEXTURE_BINDING_2D);
+  const boundTexture = gl.getParameter(gl.TEXTURE_BINDING_2D);
 
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);
 
-	if (options.src === undefined) {
-		gl.texImage2D(
-			gl.TEXTURE_2D,
-			level,
-			internalFormat,
-			options.width,
-			options.height,
-			0,
-			format,
-			type,
-			data,
-		);
-	} else {
-		gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, format, type, options.src);
-	}
+  if (options.src === undefined) {
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      level,
+      internalFormat,
+      options.width,
+      options.height,
+      0,
+      format,
+      type,
+      data,
+    );
+  } else {
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, format, type, options.src);
+  }
 
-	if (generateMipmaps) {
-		gl.generateMipmap(gl.TEXTURE_2D);
+  if (generateMipmaps) {
+    gl.generateMipmap(gl.TEXTURE_2D);
 
-		// anisotropic filtering
-		const ext = gl.getExtension("EXT_texture_filter_anisotropic");
-		if (ext && anisotropy > 1) {
-			const maxAnisotropy = gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
-			gl.texParameterf(
-				gl.TEXTURE_2D,
-				ext.TEXTURE_MAX_ANISOTROPY_EXT,
-				Math.min(maxAnisotropy, anisotropy),
-			);
-		}
-	}
+    // anisotropic filtering
+    const ext = gl.getExtension("EXT_texture_filter_anisotropic");
+    if (ext && anisotropy > 1) {
+      const maxAnisotropy = gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
+      gl.texParameterf(
+        gl.TEXTURE_2D,
+        ext.TEXTURE_MAX_ANISOTROPY_EXT,
+        Math.min(maxAnisotropy, anisotropy),
+      );
+    }
+  }
 
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilterMap[minFilter]);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilterMap[magFilter]);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapMap[wrapS]);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapMap[wrapT]);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilterMap[minFilter]);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilterMap[magFilter]);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapMap[wrapS]);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapMap[wrapT]);
 
-	gl.bindTexture(gl.TEXTURE_2D, boundTexture);
+  gl.bindTexture(gl.TEXTURE_2D, boundTexture);
 
-	return texture;
+  return texture;
 }
 
 export function loadTexture(src: string, options?: Omit<TextureOptions, "src" | "data">) {
-	return new Promise<TextureOptions>((resolve, reject) => {
-		const img = document.createElement("img");
-		const onload = () => {
-			resolve({ src: img, ...options });
-			img.removeEventListener("error", onerror);
-		};
-		const onerror = () => {
-			reject(`Failed to load texture: ${src}`);
-			img.removeEventListener("load", onload);
-		};
+  return new Promise<TextureOptions>((resolve, reject) => {
+    const img = document.createElement("img");
 
-		img.addEventListener("load", onload, { once: true });
-		img.addEventListener("error", onerror, { once: true });
+    if (src.startsWith("http") && new URL(src).origin !== window.location.origin) {
+      img.crossOrigin = "anonymous";
+    }
 
-		img.src = src;
-	});
+    const onload = () => {
+      resolve({ src: img, ...options });
+      img.removeEventListener("error", onerror);
+    };
+    const onerror = () => {
+      reject(`Failed to load texture: ${src}`);
+      img.removeEventListener("load", onload);
+    };
+
+    img.addEventListener("load", onload, { once: true });
+    img.addEventListener("error", onerror, { once: true });
+
+    img.src = src;
+  });
 }
--- lib/tests/screenshots.spec.ts
+++ lib/tests/screenshots.spec.ts
@@ -7,85 +7,86 @@ const ignoreRoutes = new Set(["pause", "dataTexture"]);
 const routesToTest = routes.filter(({ route }) => !ignoreRoutes.has(route));
 
 for (const { section, route } of routesToTest) {
-	test(route, async ({ page, baseURL }) => {
-		await page.goto(`${baseURL}/${section}/${route}`);
+  test(route, async ({ page, baseURL }) => {
+    await page.goto(`${baseURL}/${section}/${route}`);
 
-		await expect(page.locator("main")).toHaveScreenshot();
-	});
+    await expect(page.getByText("Renders: 1")).toBeVisible();
+    await expect(page.locator("main")).toHaveScreenshot();
+  });
 }
 
 test("pointer move", async ({ page, viewport, baseURL }) => {
-	await page.goto(`${baseURL}/pointer/pointer`);
+  await page.goto(`${baseURL}/pointer/pointer`);
 
-	await expect(page.getByText("Renders: 1")).toBeVisible();
+  await expect(page.getByText("Renders: 1")).toBeVisible();
 
-	await page.mouse.move((viewport?.width || 0) * 0.5, (viewport?.height || 0) * 0.45);
+  await page.mouse.move((viewport?.width || 0) * 0.5, (viewport?.height || 0) * 0.45);
 
-	await expect(page.getByText("Renders: 2")).toBeVisible();
+  await expect(page.getByText("Renders: 2")).toBeVisible();
 
-	await page.mouse.down();
+  await page.mouse.down();
 
-	await page.waitForTimeout(100);
+  await page.waitForTimeout(100);
 
-	await expect(page.locator("main")).toHaveScreenshot();
+  await expect(page.locator("main")).toHaveScreenshot();
 });
 
 test("play / pause controls - local", async ({ page, baseURL }) => {
-	await page.goto(`${baseURL}/core/pause`);
+  await page.goto(`${baseURL}/core/pause`);
 
-	await expect(page.getByText("Renders: 1")).toBeVisible();
+  await expect(page.getByText("Renders: 1")).toBeVisible();
 
-	await page.evaluate(() => {
-		return new Promise<void>((resolve) => {
-			const playPauseBtn = document.querySelector<HTMLButtonElement>(
-				".controls.local button:nth-of-type(1)",
-			)!;
+  await page.evaluate(() => {
+    return new Promise<void>((resolve) => {
+      const playPauseBtn = document.querySelector<HTMLButtonElement>(
+        ".controls.local button:nth-of-type(1)",
+      )!;
 
-			playPauseBtn.click();
+      playPauseBtn.click();
 
-			requestAnimationFrame(() => {
-				requestAnimationFrame(() => {
-					requestAnimationFrame(() => {
-						requestAnimationFrame(() => {
-							playPauseBtn.click();
-							resolve();
-						});
-					});
-				});
-			});
-		});
-	});
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+              playPauseBtn.click();
+              resolve();
+            });
+          });
+        });
+      });
+    });
+  });
 
-	await expect(page.getByText("Renders: 5")).toBeVisible();
+  await expect(page.getByText("Renders: 5")).toBeVisible();
 
-	await expect(page.locator("main")).toHaveScreenshot();
+  await expect(page.locator("main")).toHaveScreenshot();
 });
 
 test("play / pause controls - global", async ({ page, baseURL }) => {
-	await page.goto(`${baseURL}/core/pause`);
+  await page.goto(`${baseURL}/core/pause`);
 
-	await expect(page.getByText("Renders: 1")).toBeVisible();
+  await expect(page.getByText("Renders: 1")).toBeVisible();
 
-	await page.evaluate(() => {
-		return new Promise<void>((resolve) => {
-			const playPauseBtn = document.querySelector<HTMLButtonElement>(".controls.global button")!;
+  await page.evaluate(() => {
+    return new Promise<void>((resolve) => {
+      const playPauseBtn = document.querySelector<HTMLButtonElement>(".controls.global button")!;
 
-			playPauseBtn.click();
+      playPauseBtn.click();
 
-			requestAnimationFrame(() => {
-				requestAnimationFrame(() => {
-					requestAnimationFrame(() => {
-						requestAnimationFrame(() => {
-							playPauseBtn.click();
-							resolve();
-						});
-					});
-				});
-			});
-		});
-	});
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+              playPauseBtn.click();
+              resolve();
+            });
+          });
+        });
+      });
+    });
+  });
 
-	await expect(page.getByText("Renders: 5")).toBeVisible();
+  await expect(page.getByText("Renders: 5")).toBeVisible();
 
-	await expect(page.locator("main")).toHaveScreenshot();
+  await expect(page.locator("main")).toHaveScreenshot();
 });
