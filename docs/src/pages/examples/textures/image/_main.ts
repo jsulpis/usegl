83ac9fde2fb7ca6daa2bcd8ce524555762de8715
Julien Sulpis <code@jsulpis.dev>
2024-12-10 21:28:29 +0100

    docs(examples): add an example of canvas 2D texture

--- docs/src/pages/examples/textures/image/_main.ts
+++ docs/src/pages/examples/textures/image/_main.ts
@@ -1,41 +1,53 @@
 import { loadTexture, useWebGLCanvas } from "usegl";
 import "./styles.css";
 
+const vertex = /* glsl */ `
+  attribute vec2 position;
+  attribute vec2 uv;
+  uniform sampler2D uTexture;
+  uniform vec2 uResolution;
+  varying vec2 vUv;
+
+  #define CONTAIN 1
+  #define COVER 2
+  #define OBJECT_FIT CONTAIN
+
+  void main() {
+    vec2 textureResolution = vec2(textureSize(uTexture, 0));
+    float canvasRatio = uResolution.x / uResolution.y;
+    float textureRatio = textureResolution.x / textureResolution.y;
+
+    vUv = position;
+    if (OBJECT_FIT == CONTAIN ? canvasRatio > textureRatio : canvasRatio < textureRatio) {
+      vUv.x *= canvasRatio / textureRatio;
+    } else {
+      vUv.y *= textureRatio / canvasRatio;
+    }
+    vUv = (vUv + 1.0) / 2.0;
+    gl_Position = vec4(position, 0.0, 1.0);
+  }
+`;
+
+const fragment = /* glsl */ `
+  varying vec2 vUv;
+  uniform sampler2D uTexture;
+
+  void main() {
+    vec3 color = texture(uTexture, vUv).rgb;
+    color *= step(0., vUv.x) * (1. - step(1., vUv.x));
+    color *= step(0., vUv.y) * (1. - step(1., vUv.y));
+
+    gl_FragColor = vec4(color, 1.);
+  }
+`;
+
 (async () => {
   const texture = await loadTexture("https://picsum.photos/id/669/600/400");
 
   useWebGLCanvas({
     canvas: "#glCanvas",
-    fragment: /* glsl */ `
-      in vec2 vUv;
-      uniform sampler2D uTexture;
-      uniform vec2 uResolution;
-      out vec4 fragColor;
-
-      #define CONTAIN 1
-      #define COVER 2
-      #define OBJECT_FIT CONTAIN
-
-      void main() {
-        vec2 textureResolution = vec2(textureSize(uTexture, 0));
-        float canvasRatio = uResolution.x / uResolution.y;
-        float textureRatio = textureResolution.x / textureResolution.y;
-
-        vec2 uv = vUv - 0.5;
-        if (OBJECT_FIT == CONTAIN ? canvasRatio > textureRatio : canvasRatio < textureRatio) {
-          uv.x *= canvasRatio / textureRatio;
-        } else {
-          uv.y *= textureRatio / canvasRatio;
-        }
-        uv += 0.5;
-
-        vec3 color = texture(uTexture, uv).rgb;
-        color *= step(0., uv.x) * (1. - step(1., uv.x));
-        color *= step(0., uv.y) * (1. - step(1., uv.y));
-
-        fragColor = vec4(color, 1.);
-      }
-    `,
+    vertex,
+    fragment,
     uniforms: {
       uTexture: texture,
     },
