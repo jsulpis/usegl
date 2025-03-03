8547e2286b3bbcc57dfb97ff6bd7e0fc3aa83279
Julien Sulpis <code@jsulpis.dev>
2025-03-03 23:02:32 +0100

    docs: add an example of simple particules (ping pong FBO)

---
+++ docs/examples/gpgpu/particles/index.ts
@@ -0,0 +1,96 @@
+import {
+  useWebGLContext,
+  useLoop,
+  usePingPongFBO,
+  useWebGLCanvas,
+  createFloatDataTexture,
+} from "usegl";
+import "./styles.css";
+
+const { gl, canvas } = useWebGLContext("#glCanvas");
+
+const positionsFragment = /* glsl */ `
+  uniform float uDeltaTime;
+  uniform sampler2D tPositions;
+  uniform sampler2D tVelocities;
+
+  in vec2 uv;
+  out vec4 fragColor;
+
+  vec2 wrapToRange(vec2 value) {
+    vec2 shifted = value + 1.0;
+    vec2 wrapped = mod(mod(shifted, 2.0) + 2.0, 2.0);
+    return wrapped - 1.0;
+  }
+
+  void main() {
+    vec2 position = texture(tPositions, uv).xy;
+    vec2 velocity = texture(tVelocities, uv).xy;
+
+    fragColor = vec4(wrapToRange(position + velocity * uDeltaTime), 0.0, 0.0);
+  }
+`;
+
+const count = 100;
+
+const positions = usePingPongFBO(gl, {
+  fragment: positionsFragment,
+  uniforms: {
+    uDeltaTime: 0,
+    tVelocities: createFloatDataTexture(
+      Array.from({ length: count }).flatMap(() => [
+        /* R */ Math.random() * 0.2 - 0.1,
+        /* G */ Math.random() * 0.2 - 0.1,
+        /* B */ 0,
+        /* A */ 0,
+      ]),
+    ),
+  },
+  dataTexture: {
+    name: "tPositions",
+    initialData: Array.from({ length: count }).flatMap(() => [
+      /* R */ Math.random() * 2 - 1,
+      /* G */ Math.random() * 2 - 1,
+      /* B */ 0,
+      /* A */ 0,
+    ]),
+  },
+});
+
+const renderPass = useWebGLCanvas({
+  canvas,
+  vertex: `
+    uniform sampler2D uPositions;
+    in vec2 aCoords;
+
+    void main() {
+      gl_Position = vec4(texture2D(uPositions, aCoords).xy, 0, 1);
+      gl_PointSize = 20.0;
+    }
+  `,
+  fragment: `
+    in vec2 uv;
+    out vec4 outColor;
+
+    void main() {
+      vec2 uv = gl_PointCoord.xy;
+      outColor = vec4(0, 1, .5, smoothstep(0.5, 0.4, length(uv - 0.5)));
+    }
+  `,
+  uniforms: {
+    uPositions: () => positions.texture,
+  },
+  attributes: {
+    aCoords: positions.coords,
+  },
+  transparent: true,
+});
+
+// Wait for the canvas to be resized to avoid a flash at the first renders
+renderPass.onCanvasReady(() => {
+  useLoop(({ deltaTime }) => {
+    positions.uniforms.uDeltaTime = deltaTime / 500;
+    positions.render();
+    renderPass.render();
+  });
+});
