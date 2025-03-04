84ad736342da87ef8f5ca1b8665523f6189ce331
Julien Sulpis <code@jsulpis.dev>
2025-03-04 21:13:40 +0100

    chore: update docker setup for playwright tests

--- lib/playground/src/components/routes.ts
+++ lib/playground/src/components/routes.ts
@@ -1,11 +1,16 @@
 import { readdirSync } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export const sections = readdirSync("playground/src/pages", { withFileTypes: true })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const sections = readdirSync(path.join(__dirname, "../pages"), { withFileTypes: true })
   .filter((file) => file.isDirectory())
   .map((folder) => folder.name);
 
 export const routes = sections.flatMap((section) => {
-  const files = readdirSync(`playground/src/pages/${section}`);
+  const files = readdirSync(path.join(__dirname, "../pages", section));
 
   return files.map((file) => {
     const route = file.replace(".astro", "");
